115b337b80f14c6db4ccb1d9332397128254d701
javier <elchangodiaz1@gmail.com>
2020-10-28 13:47:59 -0600

    book & aboutus

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ArticleListComponent } from './article-list/article-list.component';
+import { InicioComponent } from './inicio/inicio.component';
 import { ParallaxSrcollComponent } from './parallax-srcoll/parallax-srcoll.component';
 
 
 const routes: Routes = [
-  { path: 'articles', component: ArticleListComponent}, 
-  { path: 'about', component: AboutComponent},
+  { path: 'about', component: ArticleListComponent}, 
+  { path: 'proyectos', component: AboutComponent},
   { path: '', component: ParallaxSrcollComponent},
+  { path: 'cronicas', component: InicioComponent},
 ];
 
 @NgModule({
